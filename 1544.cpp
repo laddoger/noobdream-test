250b84132258e77b1acc8d3a6b207cb3056a5b67
laddoger <129922716+laddoger@users.noreply.github.com>
2024-03-22 13:16:01 +0800

    Create 1544.cpp
    合并果子，优先队列应用

---
+++ 1544.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct cmp {
+	bool operator() (int a, int b) {
+		return a > b;
+	}
+};
+
+int main () {
+	priority_queue<int, vector<int>, cmp> q;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		q.push(x);
+	}
+	int sum = 0;
+	while (q.size() > 1) {
+		int a, b;
+		a = q.top();
+		q.pop();
+		b = q.top();
+		q.pop();
+		sum += a + b;
+		q.push(a + b);
+	}
+	cout << sum;
+	return 0;
+}
